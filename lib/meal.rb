720ac0018de012323946e2b466fe08dfbb57d73d
FletchyAF <joshua.f1998@gmail.com>
2021-02-02 09:46:25 -0500

    commit tree 481886cedd44bf6daca28c69a5f5e9829c2bd75d

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
 class Meal
 
+    attr_accessor :waiter, :customer, :total, :tip
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def meals
+        customer_meals = Meal.all.find_all {|meal| meal.customer == self}
+        customer_meals.select {|meal| self}
+    end
 end
